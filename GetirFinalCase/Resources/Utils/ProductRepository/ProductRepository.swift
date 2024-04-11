11c73ba344504e7512557097d94428db9bc37e86
khanMurat <ala.muraat07@gmail.com>
2024-04-11 17:13:31 +0300

    Realm database added

--- GetirFinalCase/Resources/Utils/ProductRepository/ProductRepository.swift
+++ GetirFinalCase/Resources/Utils/ProductRepository/ProductRepository.swift
@@ -6,86 +6,75 @@
 //
 
 import Foundation
-import SQLite
+import RealmSwift
 
 protocol ProductRepositoryProtocol {
-    
+    func addToBasket(productID:String)
+    func removeFromBasket(productID:String)
+    func clearBasket()
+    func checkIfProductInBasket(productID:String) -> Bool
+    func checkProductBasketCount(productID:String) -> Int
+    func getAllBasketItems() -> [RealmProduct]
 }
 
 final class ProductRepository {
     
     static let shared = ProductRepository()
     
-    private var database : Connection!
+    let realm = try? Realm()
     
-    let productTable = Table("Product")
-    let id = Expression<Int>("id")
-    let name = Expression<String>("name")
-    let imageURL = Expression<String>("imageURL")
-    let price = Expression<Int>("price")
-    let priceText = Expression<String>("priceText")
-    let thumbnailURL = Expression<String>("thumbnailURL")
-    let isInBasket = Expression<Bool>("isInBasket")
-    let basketCount = Expression<Int>("basketCount")
-    
-    private init() {
-        
-        getDatabasePath()
-        
-    }
-    
-    private func getDatabasePath(){
-        
-        do{
-            let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
-            
-            self.database = try Connection("\(path)/db.sqlite3")
-            
-            createTable()
-        }catch{
-            print("DEBUG : Error when database connection \(error.localizedDescription)")
-        }
-    }
-    
-    private func createTable(){
-        
-        do{
-            let existingTables = try database.scalar("SELECT name FROM sqlite_master WHERE type='table' AND name='Product'")
-            if existingTables != nil{
-                print("Table already exists")
-                return
-            }
-        }catch{
-            print("Error checking for existing table: \(error)")
-        }
-        
-        do{
-            try database.run(productTable.create { t in
-                t.column(id, primaryKey: true)
-                t.column(name)
-                t.column(price)
-                t.column(priceText)
-                t.column(imageURL)
-                t.column(thumbnailURL)
-                t.column(isInBasket)
-                t.column(basketCount)
-            })
-        }catch{
-            print(error.localizedDescription)
-        }
-        
-    }
-    
-//    func insertWord(model:DatabaseModel){
-//        
-//        let insertOrUpdate = productTable.insert(or: .replace,
-//                                               name <- model.name
-//        )
-//        do{
-//            try database.run(insertOrUpdate)
-//            print("Inserted word successfully.")
-//        }catch{
-//            print("DEBUG : Error when insert word to table \(error.localizedDescription)")
-//        }
-//    }
 }
+
+extension ProductRepository : ProductRepositoryProtocol {
+    
+    func addToBasket(productID: String) {
+        guard let product = realm?.object(ofType: RealmProduct.self, forPrimaryKey: productID) else { return }
+        try! realm?.write {
+            product.isBasket = true
+            product.basketCount += 1
+        }
+    }
+    
+    func removeFromBasket(productID: String) {
+        guard let product = realm?.object(ofType: RealmProduct.self, forPrimaryKey: productID) else { return }
+        try! realm?.write {
+            if product.basketCount > 0 {
+                product.basketCount -= 1
+                if product.basketCount == 0 {
+                    product.isBasket = false
+                }
+            }
+        }
+    }
+    
+    func clearBasket() {
+        let basketProducts = realm?.objects(RealmProduct.self).filter("isBasket == true")
+        try! realm?.write {
+            basketProducts?.forEach { product in
+                product.isBasket = false
+                product.basketCount = 0
+            }
+        }
+    }
+    
+    func checkIfProductInBasket(productID: String) -> Bool {
+        guard let product = realm?.object(ofType: RealmProduct.self, forPrimaryKey: productID) else { return false }
+        return product.isBasket
+    }
+    
+    func checkProductBasketCount(productID:String) -> Int {
+        
+        if checkIfProductInBasket(productID: productID) {
+            guard let product = realm?.object(ofType: RealmProduct.self, forPrimaryKey: productID) else { return 0 }
+            return product.basketCount
+        }else{
+            return 0
+        }
+    }
+    
+    func getAllBasketItems() -> [RealmProduct] {
+        let basketItems = realm?.objects(RealmProduct.self).filter("isBasket == true")
+        return basketItems?.compactMap({ $0 }) ?? []
+    }
+}
+
